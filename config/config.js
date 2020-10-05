0af13e610e71d3cfd441c253bbc743a3856ef36e
Aaron Young <hi@aaronyoung.dev>
2020-10-05 17:22:17 -0400

    feat: Add domain list to config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,27 @@
 require("dotenv").config();
+const validator = require("validator");
+
+const { MONGO_URI, PORT, SMTP_PORT, DOMAINS } = process.env;
+if (!DOMAINS) {
+  throw new Error(
+    "No domains specified in .env or environment variables, please specify at least one"
+  );
+}
+const domains = DOMAINS.split(",").map((domain) => domain.trim());
+domains.forEach((domain) => {
+  if (!validator.isFQDN(domain)) {
+    throw new Error(`The provided domain, '${domain}', is an invalid domain.`);
+  }
+});
 
 const configObj = {
-  mongoURI: process.env.MONGO_URI,
-  port: process.env.PORT || 5000,
-  smtpPort: process.env.SMTP_PORT || 25
+  mongoURI: MONGO_URI,
+  port: PORT || 5000,
+  smtpPort: SMTP_PORT || 25,
+  domains: domains.reduce((acc, domain) => {
+    acc[domain] = true;
+    return acc;
+  }, {}),
 };
 
 module.exports = Object.freeze(configObj);
