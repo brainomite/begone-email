8881f838fd7ac6fb55a4b9e5c007f12104f693e2
Aaron Young <hi@aaronyoung.dev>
2020-10-06 14:07:35 -0400

    feat: Add get route for specific email
    This commit will resolve #4

--- api/routes/api/mailbox/index.js
+++ api/routes/api/mailbox/index.js
@@ -1,7 +1,11 @@
+"use strict";
 const express = require("express");
+const validator = require("validator");
+const ObjectId = require("mongoose").Types.ObjectId;
 const router = express.Router();
 const CREATED = 201;
 const INTERNAL_SERVER_ERROR = 500;
+const BAD_REQUEST = 400;
 const validateEmail = require("../../../../util/validate-email");
 const sendTestEmail = require("../../../../util/send-test-email");
 const EmailBox = require("../../../../models/EmailBox");
@@ -41,5 +45,27 @@ router.post("/mailbox/:email", ({ params: { email } }, res) => {
       .send("Error sending test email to " + email);
   }
 });
+
+router.get(
+  "/mailbox/:email/:emailId",
+  async ({ params: { email, emailId } }, res) => {
+    const validationFailure = validateEmail(email); // falsy if validation succeeds
+    if (validationFailure) {
+      res.status(validationFailure.status).send(validationFailure.msg);
+      return;
+    }
+    if (!validator.isMongoId(emailId)) {
+      res.status(BAD_REQUEST).send(`${emailId} isn't in the correct format`);
+    }
+    const emailBoxWithSpecificEmail = await EmailBox.findById(email, {
+      emails: { $elemMatch: { _id: { $eq: new ObjectId(emailId) } } },
+    });
+    if (!emailBoxWithSpecificEmail.emails.length) {
+      res.status(404).send(`Specific email, ${emailId}, not found.`);
+      return;
+    }
+    res.status(200).send(emailBoxWithSpecificEmail);
+  }
+);
 
 module.exports = router;
